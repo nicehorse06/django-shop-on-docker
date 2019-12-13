a2d14b047a6c1c56cb2ab12d05522ee632e323ab
jimmyma <jimmyma@croxlink.com>
2019-12-13 11:57:43 +0800

    新增建立migrate的指令，目前dev可以跑，prod還是有問題

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -11,8 +11,10 @@ then
     echo "PostgreSQL started"
 fi
 
+python manage.py makemigrations
 python manage.py flush --no-input
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
+python manage.py loaddata */fixtures/initial_*
 
 exec "$@"
